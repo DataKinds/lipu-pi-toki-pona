0a168e67d29eed1ebce960ed3cdb595cb5f6be1e
Tyler Limkemann <tslimkemann42@gmail.com>
2020-05-05 22:17:41 -0700

    Links!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,35 @@
 class ApplicationController < ActionController::Base
-  def index
+  def landing
+  end
+
+  def around_the_internet
+    @ls = {}
+    
+    @ls['Good learning resoures'] = %w(
+      http://tokipona.org/
+      http://www.tokipona.net/
+      https://rnd.neocities.org/tokipona
+      https://github.com/stefichjo/toki-pona
+      https://tokipona.fandom.com/wiki/lipu_lawa
+    )
+
+    @ls['Dictionaries'] = %w(
+      http://tokipona.net/tp/ClassicWordList.aspx
+      https://docs.google.com/document/d/10hP3kR7mFN0E6xW3U6fZyDf7xKEEvxssM96qLq4E0ms/edit
+    )
+
+    @ls['Fonts'] = %w(
+      https://github.com/janSame/linja-pona 
+      https://github.com/davidar/linja-pona 
+    )
+
+    @ls['Active communities'] = %w(
+      https://discord.gg/XKzj3ex
+      https://reddit.com/r/tokipona
+    )
+
+    @ls['Fun and humor'] = %w(
+      https://reddit.com/r/mi_lon
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'application#home'
+  root to: 'application#landing'
+
+
+  get 'around_the_internet', to: 'application#around_the_internet'
 end
