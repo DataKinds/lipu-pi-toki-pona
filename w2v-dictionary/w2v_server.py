aafa2b3f3eaa423df749058b49c283c4f420edef
loovjo <jonathan.loov@gmail.com>
2020-05-06 19:28:52 +0200

    Add word2vec server

---
+++ w2v-dictionary/w2v_server.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import json
+from w2v_dict import load_words, load_tp_words, get_string_vector
+
+api = Flask(__name__)
+
+words, DIMS = load_words()
+
+tp_words = load_tp_words(words)
+
+@api.route('/dict/<string>', methods=['GET'])
+def dict(string):
+    print(f"Querying {string}")
+    vec = get_string_vector(words, string, verbose=True)
+
+    if vec is None:
+        return "Word not found!"
+
+    angles = [(word, word.angle_to(vec)) for word in tp_words]
+    goods = sorted(angles, key=lambda pair: pair[1])
+
+    return json.dumps([{"word": word.get_subword(), "ext": word.get_ext(), "angle": angle} for word, angle in goods])
+
+if __name__ == '__main__':
+    api.run()
